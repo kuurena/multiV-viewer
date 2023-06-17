3f3811f760d315ff03e96f4afcd4d36b9e8829ac
kuurena <kuurenaxd@gmail.com>
2023-06-17 15:44:22 +0700

    Setting up the project using vite with react and tailwind

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <div className="m-1 flex h-screen justify-center justify-items-center bg-slate-400">
+      <h1 className="pt-[25%] text-3xl font-bold underline">Hello world!</h1>
+    </div>
+  );
+}
