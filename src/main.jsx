513e7a9bbe12e0b72d7d752b51e41ae287cb7193
kuurena <kuurenaxd@gmail.com>
2023-06-21 17:20:30 +0700

    moved the button and video into the grid layout

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Grid from "./App.jsx";
 import Test from "./component/test.jsx";
+import TestApp from "./testApp.jsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Test />
+    <TestApp />
   </React.StrictMode>
 );
--- src/testApp.jsx
+++ src/testApp.jsx
@@ -1,13 +1,13 @@
-/*import React from "react";
+import React from "react";
 import { WidthProvider, Responsive } from "react-grid-layout";
 import _ from "lodash";
 import "/node_modules/react-grid-layout/css/styles.css";
 import "/node_modules/react-resizable/css/styles.css";
+import Test from "./component/test";
 
 const ResponsiveReactGridLayout = WidthProvider(Responsive);
 
-
-export default class Test extends React.PureComponent {
+export default class TestApp extends React.PureComponent {
   static defaultProps = {
     className: "layout",
     cols: { lg: 12, md: 10, sm: 6, xs: 4, xxs: 2 },
@@ -25,6 +25,7 @@ export default class Test extends React.PureComponent {
           y: 0,
           w: 2,
           h: 2,
+          minH: 50,
           add: i === list.length - 1,
         };
       }),
@@ -38,9 +39,14 @@ export default class Test extends React.PureComponent {
   createElement(el) {
     const i = el.i;
     return (
-      <div key={i} data-grid={el} className="bg-slate-300">
+      <div
+        key={i}
+        data-grid={el}
+        className="flex items-center justify-center bg-pink-200"
+      >
+        <Test />
         <button
-          className="absolute right-1 top-1 h-7 w-3 cursor-pointer rounded bg-red-400"
+          className="absolute left-1 top-1 h-7 w-3 cursor-pointer rounded bg-red-400"
           onClick={this.onRemoveItem.bind(this, i)}
         ></button>
       </div>
@@ -48,7 +54,7 @@ export default class Test extends React.PureComponent {
   }
 
   onAddItem() {
-    // eslint no-console: 0
+    /*eslint no-console: 0*/
     console.log("adding", "n" + this.state.newCounter);
     this.setState({
       // Add a new item. It must have a unique key!
@@ -98,43 +104,4 @@ export default class Test extends React.PureComponent {
       </div>
     );
   }
-}*/
-
-import React from "react";
-import axios from "axios";
-import { useState, useEffect } from "react";
-import "vidstack/styles/defaults.css";
-import { MediaOutlet, MediaPlayer } from "@vidstack/react";
-
-function Test() {
-  const [source, setSource] = useState("");
-  const [showButton, setShowButton] = useState(true);
-
-  const url =
-    "https://consumet-cyrlcx779-kuurena.vercel.app/anime/gogoanime/watch/spy-x-family-episode-1";
-  const getData = async () => {
-    try {
-      const { data } = await axios.get(url);
-      console.log(data.sources);
-      setShowButton(!showButton);
-      return setSource(data.sources[3].url);
-    } catch (err) {
-      throw new Error(err.message);
-    }
-  };
-
-  return (
-    <div className="flex h-[90%] w-[90%] items-center justify-center">
-      {showButton && (
-        <button onClick={getData} className="bg h-10 w-20 bg-pink-300">
-          Play
-        </button>
-      )}
-      <MediaPlayer title="Test" src={source} controls>
-        <MediaOutlet />
-      </MediaPlayer>
-    </div>
-  );
 }
-
-export default Test;
